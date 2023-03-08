247f11d1d6d9c568333f878d8bbc5322d04822f5
student <lukaslux@outlook.com>
2023-03-08 15:57:19 +0100

    zadanie4a

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class Main {
 
-    public static final Point test = new Point(2,3);
-
     public static void main(String[] args) {
-
 
     }
 }
