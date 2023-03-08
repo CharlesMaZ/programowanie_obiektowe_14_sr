d3937137c36c91540592c6e056e10968e33b5c84
student <lukaslux@outlook.com>
2023-03-08 14:02:42 +0100

    Po zajeciach 1

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,6 @@ public class Main {
     public static final Point test = new Point(2,3);
 
     public static void main(String[] args) {
-        Point point  = new Point(2,3);
-//<line x1="20" y1="20" x2="80" y2="80" style="stroke:black" />
 
-        String s = String.format(Locale.ENGLISH,"<line x1=\"%f\" y1=\"20\" x2=\"80\" y2=\"80\" style=\"stroke:black\" />", point.x);
-        System.out.println(s);
-
-        int array[] = new int[4];
-        int array2[] = new int[] {
-                2,3,4,5
-        };
-        array[0] = 2;
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,30 +1,49 @@
+import java.util.Locale;
+
 public class Segment {
-    Point first;
-    Point secend;
 
-    public Segment(Point first, Point secend) {
-        this.first = first;
-        this.secend = secend;
+    private final Point p1;
+    private final Point p2;
+    public Segment (Point p1,Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
-    public Point getFirst() {
-        return first;
+    public Point getP1() {
+        return p1;
     }
 
-    public void setFirst(Point first) {
-        this.first = first;
+    public Point getP2() {
+        return p2;
     }
 
-    public Point getSecend() {
-        return secend;
+    public float distance()
+    {
+        return (float) Math.hypot(p1.x - p2.x, p1.y - p2.y);
     }
 
-    public void setSecend(Point secend) {
-        this.secend = secend;
+    public String toSVG(){
+        return String.format(Locale.ENGLISH, "<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />",p1.x,p1.y,p2.x,p2.y);
     }
 
-    public double getDist () {
-        return Math.hypot(first.x - secend.x, first.y - secend.y);
-//        return Math.sqrt(Math.pow(first.x - secend.x, 2) + Math.pow(first.y - secend.y, 2) );
+    public static Segment[] perpendicular(Segment line, Point point, double r) {
+        double a;
+        a = (line.p1.y - line.p2.y) / (line.p1.x - line.p2.x);
+        double b;
+        a=-1/a;
+        b=point.y-a*point.x;
+
+        double x0 = point.x;
+        double y0 = point.y;
+        //double r = line.distance();
+
+        double root = Math.sqrt(-y0*y0+(2*a*x0+2*b)*y0-a*a*x0*x0-2*a*b*x0+(a*a+1)*r*r-b*b);
+        double x1 = -(root-a*y0-x0+a*b)/(a*a+1);
+        double y1 = -(a*root-a*a*y0-a*x0-b)/(a*a+1);
+        double x2 = (root+a*y0+x0-a*b)/(a*a+1);
+        double y2 = (a*root+a*a*y0+a*x0+b)/(a*a+1);
+
+        return new Segment[]{new Segment(point, new Point(x1,y1)), new Segment(point, new Point(x2,y2))};
     }
 }
