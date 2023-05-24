ae8b5b9d47fb8b54c413730c4613e996bb8ffb4e
Your Name <you@example.com>
2023-05-24 15:27:45 +0200

    lab11 z2

--- lab_11_client/lab_11_client/src/main/java/com/example/lab_11_client/HelloApplication.java
+++ lab_11_client/lab_11_client/src/main/java/com/example/lab_11_client/HelloApplication.java
@@ -3,18 +3,37 @@ package com.example.lab_11_client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        ConnectionThread connectionThread = new ConnectionThread("localhost", 5000);
+        connectionThread.start();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Logowanie");
+        dialog.setContentText("Wpisz login");
+
+        Optional<String> result = dialog.showAndWait();
+
+        if(result.isPresent()) {
+            String login = result.get();
+            connectionThread.login(login);
+
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+
     }
 
     public static void main(String[] args) {
