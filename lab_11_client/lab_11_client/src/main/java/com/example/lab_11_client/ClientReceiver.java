4354125398496066a0a4a23b9ac3b07d2cfaee06
Łukasz Kurant <lukaslux@outlook.com>
2023-05-30 07:24:31 +0200

    lab12 prep

--- lab_11_client/lab_11_client/src/main/java/com/example/lab_11_client/ClientReceiver.java
+++
@@ -1,5 +0,0 @@
-package com.example.lab_11_client;
-
-public interface ClientReceiver {
-    void receiveBroadcast(String sender, String message);
-}
--- lab_11_client/lab_11_client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab_11_client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.lab_11_client to javafx.fxml;
-    exports com.example.lab_11_client;
-}
