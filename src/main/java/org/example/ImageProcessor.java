ec01dc33d98f7cedeee7e3a7c449fca50e6f40b4
Your Name <you@example.com>
2023-06-07 14:42:08 +0200

    lab13 z2

--- src/main/java/org/example/ImageProcessor.java
+++ src/main/java/org/example/ImageProcessor.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +19,31 @@ public class ImageProcessor {
 
     public void saveImage(String path) throws IOException {
         ImageIO.write(image, "png", new File(path));
+    }
+
+    public int getPixelAfterBright(int oldColor, int factor) {
+        return 0;
+    }
+
+    public void setBrightness(int factor) {
+
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                Color color = new Color(image.getRGB(x,y));
+
+                int red = color.getRed() + factor;
+                int green = color.getGreen() + factor;
+                int blue = color.getBlue() + factor;
+
+                red = Math.max(0, Math.min(255, red));
+                green = Math.max(0, Math.min(255, green));
+                blue = Math.max(0, Math.min(255, blue));
+
+                Color newColor = new Color(red, green, blue);
+                image.setRGB(x, y, newColor.getRGB());
+
+            }
+        }
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+//    int color = image.getRGB(0,0);
+//
+//    int r = (color >> 16) & 0xFF;
+//    int g = (color >> 8) & 0xFF;
+//    int b = color & 0xFF;
+//
+//    r += 100;
+//    g += 100;
+//    b += 100;
+//
+//    int result = (r << 16) | (g << 8) | b;
+//
+//        image.setRGB(0,0,result);
+//
+//    Color color1 = new Color(color);
+//        color1.getRed();
+//        new Color(color1.getRed(), color1.getGreen(), color1.getBlue());
+
+
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        String path = "C:\\Users\\student\\Projects\\lab13\\programowanie_obiektowe_14_sr\\R.jpg";
+        String path2 = "C:\\Users\\student\\Projects\\lab13\\programowanie_obiektowe_14_sr\\R2.png";
+        ImageProcessor imageProcessor = new ImageProcessor();
+        imageProcessor.loadImage(path);
+        imageProcessor.setBrightness(50);
+        imageProcessor.saveImage(path2);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
